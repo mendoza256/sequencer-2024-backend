5f0ef29bf5bf0c2132b31de3778bef64815e066f
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-03-16 17:33:30 +0100

    add auth0 - add endpoints - add model - add middleware

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,84 +1,130 @@
 package controllers
 
 import (
-	"fmt"
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
 	"net/http"
+	"net/url"
+	"os"
 
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"github.com/mendoza256/sequencer-2024-backend/db"
-	"github.com/mendoza256/sequencer-2024-backend/lib"
-	"github.com/mendoza256/sequencer-2024-backend/models"
+	"golang.org/x/oauth2"
 )
 
 
-func LoginHandler(c *gin.Context) {
-    var user models.User
-    err := c.BindJSON(&user)
-    if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-
-    var dbUser models.User
-    if db.DB != nil {
-        result := db.DB.Where("email = ?", user.Email).First(&dbUser)
-        if result.Error != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{
-                "error": result.Error.Error(),
-            })
-            return
-        }
-    } else {
-        fmt.Println("db is nil")
-    }
-
-    if lib.CheckPasswordHash(user.Password, dbUser.Password) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Login successful",
-        })
-    } else {
-        c.JSON(http.StatusUnauthorized, gin.H{
-            "message": "Invalid credentials",
-        })
-    }
+type Authenticator struct {
+	*oidc.Provider
+	oauth2.Config
 }
 
-func SignUpHandler(c *gin.Context) {
-    var user models.User
-    err := c.BindJSON(&user)
-    if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
+// New instantiates the *Authenticator.
+func New() (*Authenticator, error) {
+	provider, err := oidc.NewProvider(
+		context.Background(),
+		"https://"+os.Getenv("AUTH_DOMAIN")+"/",
+	)
+	if err != nil {
+		return nil, err
+	}
 
-    hashedPassword, err := lib.HashPassword(user.Password)
+	conf := oauth2.Config{
+		ClientID:     os.Getenv("AUTH_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("AUTH_CALLBACK_URL"),
+		Endpoint:     provider.Endpoint(),
+		Scopes:       []string{oidc.ScopeOpenID, "profile"},
+	}
 
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-
-    user.Password = hashedPassword
-
-    if db.DB != nil {
-        result := db.DB.Create(&user)
-        if result.Error != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{
-                "error": result.Error.Error(),
-            })
-            return
-        }
-    } else {
-        fmt.Println("db is nil")
-    }
-
-    c.JSON(http.StatusOK, gin.H{
-        "message": "User saved successfully",
-    })
+	return &Authenticator{
+		Provider: provider,
+		Config:   conf,
+	}, nil
 }
+
+// VerifyIDToken verifies that an *oauth2.Token is a valid *oidc.IDToken.
+func (a *Authenticator) VerifyIDToken(ctx context.Context, token *oauth2.Token) (*oidc.IDToken, error) {
+	rawIDToken, ok := token.Extra("id_token").(string)
+	if !ok {
+		return nil, errors.New("no id_token field in oauth2 token")
+	}
+
+	oidcConfig := &oidc.Config{
+		ClientID: a.ClientID,
+	}
+
+	return a.Verifier(oidcConfig).Verify(ctx, rawIDToken)
+}
+
+// LOGIN
+func LoginHandler(auth *Authenticator) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		state, err := generateRandomState()
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		// Save the state inside the session.
+		session := sessions.Default(ctx)
+		session.Set("state", state)
+		if err := session.Save(); err != nil {
+			ctx.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		ctx.Redirect(http.StatusTemporaryRedirect, auth.AuthCodeURL(state))
+	}
+}
+
+func generateRandomState() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	state := base64.StdEncoding.EncodeToString(b)
+
+	return state, nil
+}
+
+
+// USER
+func UserHandler(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+	profile := session.Get("profile")
+
+	ctx.HTML(http.StatusOK, "user.html", profile)
+}
+
+// LOGOUT
+func LogoutHandler(ctx *gin.Context) {
+	logoutUrl, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/v2/logout")
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	scheme := "http"
+	if ctx.Request.TLS != nil {
+		scheme = "https"
+	}
+
+	returnTo, err := url.Parse(scheme + "://" + ctx.Request.Host)
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	parameters := url.Values{}
+	parameters.Add("returnTo", returnTo.String())
+	parameters.Add("client_id", os.Getenv("AUTH0_CLIENT_ID"))
+	logoutUrl.RawQuery = parameters.Encode()
+
+	ctx.Redirect(http.StatusTemporaryRedirect, logoutUrl.String())
+}
+
--- server.go
+++ server.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"encoding/gob"
+	"log"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/mendoza256/sequencer-2024-backend/controllers"
 	"github.com/mendoza256/sequencer-2024-backend/db"
+	"github.com/mendoza256/sequencer-2024-backend/middleware"
 )
 
 
 func main() {
     db.InitDb()
+    if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to load the env vars: %v", err)
+	}
 
     router := gin.Default()
-    router.POST("/save-sequence", controllers.SaveSequenceHandler)
-    router.GET("/get-user-sequences", controllers.GetUserSequencesHandler)
-    router.POST("/auth/signup", controllers.SignUpHandler)
-    router.POST("/auth/login", controllers.LoginHandler)
+
+    // To store custom types in our cookies,
+	// we must first register them using gob.Register
+	gob.Register(map[string]interface{}{})
+    store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("auth-session", store))
+
+    auth, err := controllers.New()
+	if err != nil {
+		log.Fatalf("Failed to initialize the authenticator: %v", err)
+	}
+    
+	router.GET("/login", controllers.LoginHandler(auth))
+	router.GET("/callback", controllers.LoginHandler(auth))
+	router.GET("/logout", controllers.LogoutHandler)
+    router.GET("/user", middleware.IsAuthenticated, controllers.UserHandler)
 
     router.Run(":8080")
 }
